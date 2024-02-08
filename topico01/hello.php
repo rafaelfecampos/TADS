f99f29c3991df0cfd92616ae303a7bd68a69f661
Rafael <2001campos.rafael@gmail.com>
2024-02-08 20:52:46 -0300

    aula01

---
+++ topico01/hello.php
@@ -0,0 +1,14 @@
+<!-- <?php
+    print "Alô Mundo!";
+    print("Alô Mundo funcao com print");
+
+    print 'Alô Mundo aspas simples';
+
+    echo "Alô com echo";
+    echo("Echo com função!");
+?> -->
+
+<?php
+    echo"Faculdades Integradas <b>Vianna Junior</b><br>";
+    echo"Disciplina de:\n"; //\n nao quebra linha no cliente porque o php gera um html que é interpretado pelo navegador
+    echo"Linguagem de programação Web";
